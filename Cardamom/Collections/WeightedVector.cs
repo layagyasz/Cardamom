828006311b2d7e5713b3ab636cc1ae7eeb0f9863
layagyasz <zchrylawrence@gmail.com>
2022-11-07 20:56:59 -0700

    Update WeightVector style

--- Cardamom/Collections/WeightedVector.cs
+++ Cardamom/Collections/WeightedVector.cs
@@ -4,31 +4,31 @@ namespace Cardamom.Collections
     {
         double[] _values;
         T[] _keys;
-        int _Alloc;
+        int _alloc;
 
         public double Total { get; private set; }
         public int Count { get; private set; }
-        public bool IsReadOnly { get; } = false;
+        public bool IsReadOnly => false;
 
         public WeightedVector()
         {
             Count = 0;
-            _Alloc = 1;
+            _alloc = 1;
             _values = new double[1];
             _keys = new T[1];
         }
 
-        public WeightedVector(WeightedVector<T> Copy)
+        public WeightedVector(WeightedVector<T> copy)
         {
-            Count = Copy.Count;
-            _Alloc = Copy._Alloc;
-            _values = new double[_Alloc];
-            _keys = new T[_Alloc];
-            Total = Copy.Total;
-            for (int i = 0; i < _Alloc; i++)
+            Count = copy.Count;
+            _alloc = copy._alloc;
+            _values = new double[_alloc];
+            _keys = new T[_alloc];
+            Total = copy.Total;
+            for (int i = 0; i < _alloc; i++)
             {
-                _keys[i] = Copy._keys[i];
-                _values[i] = Copy._values[i];
+                _keys[i] = copy._keys[i];
+                _values[i] = copy._values[i];
             }
         }
 
@@ -45,86 +45,98 @@ namespace Cardamom.Collections
             return GetEnumerator();
         }
 
-        public void Add(double Weight, T Value)
+        public void Add(double weight, T value)
         {
-            if (Count + 1 == _Alloc)
+            if (Count + 1 == _alloc)
             {
-                _Alloc *= 2;
-                T[] newK = new T[_Alloc];
-                double[] newA = new double[_Alloc];
+                _alloc *= 2;
+                T[] newK = new T[_alloc];
+                double[] newA = new double[_alloc];
                 Array.Copy(_keys, newK, _keys.Length);
                 Array.Copy(_values, newA, _keys.Length);
                 _values = newA;
                 _keys = newK;
             }
-            Total += Weight;
-            _keys[Count] = Value;
-            _values[Count + 1] = _values[Count] + Weight;
+            Total += weight;
+            _keys[Count] = value;
+            _values[Count + 1] = _values[Count] + weight;
             ++Count;
         }
 
-        public void Add(KeyValuePair<T, double> Value)
+        public void Add(KeyValuePair<T, double> value)
         {
-            Add(Value.Value, Value.Key);
+            Add(value.Value, value.Key);
         }
 
-        private int IndexOf(double V)
+        private int IndexOf(double interval)
         {
-            if (Count == 0) throw new Exception("Index on Empty WeightVector<T>");
-            if (Count == 1) return 0;
+            if (Count == 0)
+            {
+                throw new Exception("Index on Empty WeightVector<T>");
+            }
+            if (Count == 1)
+            {
+                return 0;
+            }
             int i = 0;
             int j = Count - 1;
             int c = (j + i) / 2;
             while (j - i > 1)
             {
-                if (V > _values[c])
+                if (interval > _values[c])
                 {
                     i = c;
                     c = (j + i) / 2;
                 }
-                else if (V < _values[c])
+                else if (interval < _values[c])
                 {
                     j = c;
                     c = (j + i) / 2;
                 }
                 else break;
             }
-            if (j - i == 1 && _values[c] > V) c--;
-            else if (j - i == 1 && _values[c + 1] < V) c++;
+            if (j - i == 1 && _values[c] > interval)
+            {
+                c--;
+            }
+            else if (j - i == 1 && _values[c + 1] < interval)
+            {
+                c++;
+            }
             return c;
         }
 
-        public T this[double V]
+        public T this[double interval]
         {
             get
             {
-                return _keys[IndexOf(V * Total)];
+                return _keys[IndexOf(interval * Total)];
             }
             set
             {
-                _keys[IndexOf(V * Total)] = value;
+                _keys[IndexOf(interval * Total)] = value;
             }
         }
 
         public void Clear()
         {
             Count = 0;
-            _Alloc = 1;
+            _alloc = 1;
             _values = new double[1];
             _keys = new T[1];
         }
 
-        public bool Contains(KeyValuePair<T, double> Value)
+        public bool Contains(KeyValuePair<T, double> value)
         {
             throw new NotImplementedException();
         }
 
-        public bool Remove(KeyValuePair<T, double> Value)
+        public bool Remove(KeyValuePair<T, double> value)
         {
             throw new NotImplementedException();
         }
 
-        public void CopyTo(KeyValuePair<T, double>[] Values, int Index)
+        public void CopyTo(KeyValuePair<T, double>[] values, int index)
         {
             throw new NotImplementedException();
         }
