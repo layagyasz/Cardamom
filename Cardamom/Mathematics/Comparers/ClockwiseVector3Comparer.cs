32dde2bb935697d16caf95cd300b1b7f21376fba
layagyasz <zchrylawrence@gmail.com>
2023-03-27 16:15:44 -0600

    Fix cases in clockwise sorting where angle >pi

--- Cardamom/Mathematics/Comparers/ClockwiseVector3Comparer.cs
+++ Cardamom/Mathematics/Comparers/ClockwiseVector3Comparer.cs
@@ -6,16 +6,35 @@ namespace Cardamom.Mathematics.Comparers
     {
         public Vector3 Center { get; }
         public Vector3 Normal { get; }
+        public Vector3 Reference { get; }
 
-        public ClockwiseVector3Comparer(Vector3 center, Vector3 normal)
+        public ClockwiseVector3Comparer(Vector3 center, Vector3 normal, Vector3 reference)
         {
             Center = center;
             Normal = normal;
+            Reference = reference;
         }
 
         public int Compare(Vector3 a, Vector3 b)
         {
-            return Vector3.Dot(Normal, Vector3.Cross(a - Center, b - Center)) < 0 ? 1 : -1;
+            var ua = a - Center;
+            var ub = b - Center;
+            var ha = Vector3.Dot(ua, Reference);
+            var hb = Vector3.Dot(ub, Reference);
+
+            if (hb <= 0 && ha > 0)
+            {
+                return 1;
+            }
+            if (ha <= 0 && hb > 0)
+            {
+                return -1;
+            }
+            if (ha == 0 && hb == 0)
+            {
+                return Vector3.Dot(ua, Reference) > 0 && Vector3.Dot(ub, Reference) < 0 ? -1 : 1;
+            }
+            return Vector3.Dot(Normal, Vector3.Cross(ua, ub)) > 0 ? -1 : 1;
         }
     }
 }
