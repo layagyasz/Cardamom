f34f3d1ab8da614d47a3f1d4976c0af97bc17d33
layagyasz <zchrylawrence@gmail.com>
2024-09-19 19:55:13 -0600

    IList shuffle extension

--- Cardamom/Collections/Extensions.cs
+++ Cardamom/Collections/Extensions.cs
@@ -34,6 +34,19 @@
             return min;
         }
 
+        public static void Shuffle<T>(this IList<T> list, Random random)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
         public static EnumSet<T> ToEnumSet<T>(this IEnumerable<T> source)
             where T : Enum
         {
